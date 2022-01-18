b9bcca06cab245027b95432447b850d0e230df80
AneCode <75664942+AneCode@users.noreply.github.com>
2022-01-18 17:41:30 +0530

    Update sametree.cpp

--- Tree/sametree.cpp
+++ Tree/sametree.cpp
@@ -11,30 +11,27 @@
  */
 class Solution {
 public:
-    bool  solve( TreeNode* p,TreeNode *q,bool v){
+      bool  solve( TreeNode* p,TreeNode *q,bool v){
         if(p==NULL && q==NULL)
            return v;
         
         if(v==false)
          return v;
         
-        if(p==NULL q==NULL)
+        if(p==NULL|| q==NULL)
             return false;
         if(p->val !=  q->val )
             return false;
           
-        cout<<"v:"<<v<<endl;
+        
          bool x=solve(p->left,q->left,v)==false?false:v;
-         bool y=solve(q->right,q->right,v)==false?false:v;
-        cout<<"x"<<x;
-        cout<<"y"<<y<<endl;
+         bool y=solve(p->right,q->right,v)==false?false:v;
+      
          v=(x==false || y==false)?false:v; 
         return v;
             
     } 
     bool isSameTree(TreeNode* p, TreeNode* q) {
-      if(p==NULL && q==NULL)
-          return false;
      return solve(p,q,true);
     }
 };
