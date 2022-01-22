b707cc86fd20bc5d64b3ee936b2d616f80e3a291
AneCode <75664942+AneCode@users.noreply.github.com>
2022-01-22 23:55:35 +0530

    Update 132-pattern.cpp

--- Stack/132-pattern.cpp
+++ Stack/132-pattern.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-       int k=-1e7; 
-       stack<int>s;
-           for(int i=nums.size()-1;i>=0;i--){
-               if(nums[i]<k)
-                   return true;
-               else if(!s.empty() and nums[i]>s.top()){
-                   k=s.top();
-                   s.pop();
-               }
-               else
-                   s.push(nums[i]);
-           }
+        
+        int k = INT_MIN;
+        
+        stack<int> s;
+        
+        for(int i = nums.size() - 1; i >= 0; i--) {
+            
+            if(nums[i] < k) {
+                return true;
+            }
+            
+            while(s.empty() == false and nums[i] > s.top()) {
+                k = s.top();
+                s.pop();
+            }
+            
+            s.push(nums[i]);
+        }
+        
+        
         return false;
     }
 };
